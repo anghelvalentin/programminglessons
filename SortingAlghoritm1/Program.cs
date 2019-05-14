c80350ffde45511147d921dcb83bb25197921769
Anghel Valentin <valentin_anghel_2005@hotmail.com>
2019-05-14 08:20:05 +0300

    Sorting1

---
+++ SortingAlghoritm1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SortingAlghoritm1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 32, 12, 1, 2, 46, 21, 2, 3 };
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int aux = array[i];
+                        array[i] = array[j];
+                        array[j] = aux;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
