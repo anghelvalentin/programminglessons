fd509b58b31b7c1e08125d82a16123e297f31156
vali <vanghel@jvm.ro>
2019-09-03 15:10:41 +0300

    Operators

---
+++ Operators/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operators
+{
+    class Car
+    {
+        public int Speed { get; set; }
+
+        public int IncreaseSpeed(int increase)
+        {
+            Speed += increase;
+            return Speed;
+        }
+    }
+}
---
+++ Operators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = null;
+            car?.IncreaseSpeed(10); //this statement returns null
+            car = new Car();
+            car?.IncreaseSpeed(10);
+            Console.WriteLine(car?.Speed);//10
+
+            car = null;
+            Console.WriteLine(car?.Speed ?? 0); //0
+
+            car = new Car();
+            car.Speed = 100;
+            Console.WriteLine(car?.Speed??0);//100
+
+            Car[] cars = new Car[3];
+            
+
+            Console.WriteLine(nameof(car.Speed)+"="+car.Speed); //nameof express =>it prints Speed=100
+
+            Console.WriteLine($"{nameof(car.Speed)}={car.Speed}"); //string interpolation + nameof expression
+            int i = new int();
+        }
+    }
+}
