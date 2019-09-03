a194e67e0fc20f5c54e45ec5b29a3d66129d11a3
vali <vanghel@jvm.ro>
2019-09-03 12:26:54 +0300

    Overflow Exception

---
+++ OverflowException/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OverflowException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Int32.MaxValue +1
+            int rezultat = Int32.MaxValue;
+            rezultat += 1;
+            Console.WriteLine(rezultat);
+
+
+            //Checked and OverflowException
+            try
+            {
+                checked
+                {
+                    for (int i = 1; i <= Int32.MaxValue; i++)
+                    {
+
+                    }
+                    Console.WriteLine("Does it print?");
+                }
+            }
+            catch (System.OverflowException e)
+            {
+                Console.WriteLine("Hurrey. The exception was raised and caught");
+            }
+        }
+    }
+}
