15089e0fbdccc99343886e8e36443e2d5d2beb7a
vali <vanghel@jvm.ro>
2019-09-15 10:34:20 +0300

    StackOverflow

---
+++ StackOverflowCPlusPlus/Source.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#define dimension 100000000
+
+void main()
+{
+	int arr[dimension];
+	int arr2[dimension]; 
+	printf("Hello world"); //This printf doesn't print because we will receive a exception
+}
---
+++ StackOverflowException/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackOverflowException
+{
+    class Program
+    {
+        static void RecursiveMethod()
+        {
+            RecursiveMethod();
+        }
+
+        static void Main(string[] args)
+        {
+            RecursiveMethod();
+        }
+    }
+}
